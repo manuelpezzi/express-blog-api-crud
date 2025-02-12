a4338acd779e68659fc4e886eec3eb268cc396b8
manuelpezzi <pezzimanuel@gmail.com>
2025-02-12 13:24:30 +0100

    function show

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,5 +4,18 @@ function index(req, res) {
     res.json(arreyPosts)
 }
 function show(req, res) {
+    const id = parseInt(req.params.id)
 
+    const post = arreyPost.find(post => post.id === id);
+
+    res.status(404)
+
+
+    if (!post) {
+        return res.json({
+            erorr: "Not Found",
+            message: "post non trovato"
+        });
+    }
+    res.json(post)
 }
