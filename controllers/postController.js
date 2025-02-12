9744d39f70cc6faba0c9e0add13fd90c8f45ce6e
manuelpezzi <pezzimanuel@gmail.com>
2025-02-12 13:29:22 +0100

    fucntion terminate e esportate

--- controllers/postController.js
+++ controllers/postController.js
@@ -19,3 +19,32 @@ function show(req, res) {
     }
     res.json(post)
 }
+function store(req, res) {
+    res.send('Creazione nuova pizza');
+}
+function update(req, res) {
+    res.send('Modifica integrale della pizza ' + req.params.id);
+}
+function patch(req, res) {
+    res.send('Modifica parziale della pizza ' + req.params.id);
+}
+function destroy(req, res) {
+    const id = parseInt(req.params.id)
+
+    const post = arreyPost.find(post => post.id === id);
+
+    res.status(404)
+
+
+    if (!post) {
+        return res.json({
+            erorr: "Not Found",
+            message: "post non trovato"
+        });
+    }
+    arreypost.splice(arreypost.indexOf(post), 1)
+
+    res.sendStatus(204)
+
+};
+module.exports = { index, show, store, update, patch, destroy }
